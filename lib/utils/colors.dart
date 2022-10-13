4bd97a5fbc1970225fe09cb8490b284a1d51b067
dev-apiarist <klesliedev@gmail.com>
2022-10-13 13:11:48 -0500

    home-page

---
+++ lib/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color mainPurple = Color(0xFF22223B);
+  // static const Color mainBrown = Color(0xFF3C4573);
+
+
+}
---
+++ lib/utils/dimensions.dart
@@ -0,0 +1,7 @@
+import 'package:get/get.dart';
+
+class Dimensions {
+  static double screenHeight = Get.context!.height; //868
+  static double screenWidth = Get.context!.width; //411
+
+}
