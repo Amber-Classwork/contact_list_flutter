a5db6bbca1e48137de4ba179dbc777b5e3c17215
aldaine-AG <aldaineclarke1@gmail.com>
2022-10-13 11:50:32 -0500

    created contacts model and updated user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,17 +1,15 @@
 class User {
   final String username;
   final String password;
-  final String email;
-  final String phone;
+  final String role;
 
-  const User({required this.username, required this.password, required this.phone, required this.email});
+  const User({required this.username, required this.password, required this.role});
 
   factory User.fromJSON(Map<String, dynamic> json){
     return User(
       username: json['username'],
       password: json['password'],
-      email: json['email'],
-      phone: json['phone'],
+      role: json['role'],
     );
   }
 
