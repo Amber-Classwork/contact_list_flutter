d6f4536dd005e3ac5df187a2799ebdde1418e2d8
aldaine-AG <aldaineclarke1@gmail.com>
2022-10-14 07:06:22 -0500

    updated the contact model with correct field variables

--- lib/models/contact.dart
+++ lib/models/contact.dart
@@ -3,17 +3,19 @@ class Contact {
   final String first_name;
   final String last_name;
   final String email;
-  final String phone;
+  final int contact_num;
+  final String thumbnail;
 
-  const Contact({required this.first_name, required this.last_name, required this.phone, required this.email, required this.id});
+  const Contact({required this.first_name, required this.last_name, required this.contact_num, required this.email, required this.id,required this.thumbnail});
 
-  factory Contact.fromJSON(Map<String, dynamic> json){
+  factory Contact.fromJson(Map<String, dynamic> json){
     return Contact(
       id: json['_id'],
       first_name: json['first_name'],
       last_name: json['last_name'],
       email: json['email'],
-      phone: json['phone'],
+      thumbnail: json['thumbnail'],
+      contact_num: json['contact_num'],
     );
   }
 }
